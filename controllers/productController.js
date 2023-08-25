56d873d56782823192e3c34ff4a08567e1509dce
juliadiaguileva <jvdiaguileva@gmail.com>
2023-08-25 17:55:14 +0200

    changes text

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,21 +1,18 @@
-//And this is model
 const Product = require("../models/productModel");
 const asyncHandler = require("express-async-handler");
 
-//This is controller
-//Get all products
-const getProducts = async (req, res) => {
+// get all product
+const getProducts = asyncHandler(async (req, res) => {
   try {
     const products = await Product.find({});
     res.status(200).json(products);
   } catch (error) {
-    console.log(error.message);
-    res.status(500).json({ message: error.message });
+    res.status(500);
+    throw new Error(error.message);
   }
-};
+});
 
-//Get a single product
-//create function
+// get a single product
 const getProduct = asyncHandler(async (req, res) => {
   try {
     const { id } = req.params;
@@ -24,36 +21,33 @@ const getProduct = asyncHandler(async (req, res) => {
   } catch (error) {
     res.status(500);
     throw new Error(error.message);
-    //res.status(500).json({ message: error.message });
   }
 });
 
-//refect the code to make it more clean,
-//create the product
+// create a product
 const createProduct = asyncHandler(async (req, res) => {
-  //console.log(req.body);
-  //res.send(req.body);
   try {
-    const products = await Product.create(req.body);
-    res.status(200).json(products);
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
   } catch (error) {
     res.status(500);
     throw new Error(error.message);
   }
 });
 
+// update a product
 const updateProduct = asyncHandler(async (req, res) => {
   try {
     const { id } = req.params;
     const product = await Product.findByIdAndUpdate(id, req.body);
+    // we cannot find any product in database
     if (!product) {
       res.status(404);
-      throw new Error("cannot find product with ID ${id}");
+      throw new Error(`cannot find any product with ID ${id}`);
     }
-    const updatedProduct = await Product.findById;
+    const updatedProduct = await Product.findById(id);
     res.status(200).json(updatedProduct);
   } catch (error) {
-    //console.log(error.message);
     res.status(500);
     throw new Error(error.message);
   }
@@ -65,10 +59,7 @@ const deleteProduct = asyncHandler(async (req, res) => {
     const product = await Product.findByIdAndDelete(id);
     if (!product) {
       res.status(404);
-      throw new Error("cannot find product with ID ${id}");
-      //return res
-      //  .status(404)
-      // .json({ message: "cannot find product with ID ${id}" });
+      throw new Error(`cannot find any product with ID ${id}`);
     }
     res.status(200).json(product);
   } catch (error) {
@@ -77,7 +68,6 @@ const deleteProduct = asyncHandler(async (req, res) => {
   }
 });
 
-//export it out
 module.exports = {
   getProducts,
   getProduct,
--- server.js
+++ server.js
@@ -1,22 +1,15 @@
-//Paste in server.js file to access .env file
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const productRoute = require("./routes/productRoute");
 const errorMiddleware = require("./middleware/errorMiddleware");
-const cors = require("cors");
-// no need this anymore const Product = require("./models/productModel");
+var cors = require("cors");
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-//create variable to store Variable from .env file
 const MONGO_URL = process.env.MONGO_URL;
-//how can you acces this through browser?you have to declare/set up a route.
-//app.get('/',(req, res)) request is what client sends to you and response is what you respond back to the client
 const FRONTEND = process.env.FRONTEND;
-//middleware
-//you can also use the body parser npm package instead of app.use(express.urlencoded({ extended: false }));
-//https://stackoverflow.com/questions/23259168/what-are-express-json-and-express-urlencoded
 
 var corsOptions = {
   origin: FRONTEND,
@@ -28,26 +21,26 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 //routes
+
 app.use("/api/products", productRoute);
+
 app.get("/", (req, res) => {
-  //throw new Error("fake error");
   res.send("Hello NODE API");
 });
 
 app.get("/blog", (req, res) => {
-  res.send(req.body);
+  res.send("Hello Blog");
 });
 
 app.use(errorMiddleware);
 
-//"mongodb+srv://admin:admin@cluster0.3zhvvw9.mongodb.net/Node-API?retryWrites=true&w=majority"
 mongoose.set("strictQuery", false);
 mongoose
   .connect(MONGO_URL)
   .then(() => {
-    console.log("connected to mongoDB");
+    console.log("connected to MongoDB");
     app.listen(PORT, () => {
-      console.log("Node is running on port ${PORT}");
+      console.log(`Node API app is running on port ${PORT}`);
     });
   })
   .catch((error) => {
