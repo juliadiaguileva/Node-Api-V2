45ac6e73b73f016889a81c5591e51ddda80bc68c
juliadiaguileva <jvdiaguileva@gmail.com>
2023-08-30 17:51:38 +0200

    chenges

--- middleware/errorMiddleware.js
+++ middleware/errorMiddleware.js
@@ -1,5 +1,6 @@
 //Middleware is an callback function, it can detect request, respond error in the application.
 const errorMiddleware = (err, req, res, next) => {
+  //here is middle mean it's bean activated succesfully
   console.log("here is an middleware");
   const statusCode = res.statusCode ? res.statusCode : 500;
   res.status(statusCode);
